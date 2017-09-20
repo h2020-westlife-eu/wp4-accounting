b2e65710385927bb22d22402b6a918e3b46c004a
marcoverl <marco.verlato@pd.infn.it>
2017-09-20 16:31:29 +0200

    Create westlife-accounting.sh

---
+++ westlife-accounting.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+        echo "Insert time range YYYY/MM YYYY/MM" 
+        echo "Example : $0 2015/11 2017/8"
+        exit
+fi
+start=$1
+end=$2
+echo
+echo HTC Platform KPIs:
+echo Normalized CPU Hours used=$(python accounting.py -i https://accounting.egi.eu/egi/htc/normcpu-hour/SITE/VO/$start/$end/custom-enmr.eu/onlyinfrajobs/CSV 2>/dev/null  | grep Total | grep -v'enmr.eu' | cut -d',' -f3)
+echo Number of jobs submitted=$(python accounting.py -i https://accounting.egi.eu/egi/htc/njobs/SITE/VO/$start/$end/custom-enmr.eu/onlyinfrajobs/CSV 2>/dev/null | grep Total | grep -v'enmr.eu' | cut -d',' -f3)
+echo
+echo Cloud Platform KPIs:
+echo Wall Time Hours used=$(python accounting.py -i https://accounting.egi.eu/egi/cloud/sum_elap-hour/SITE/VO/$start/$end/custom-enmr.eu/onlyinfrajobs/CSV 2>/dev/null | grep Total | grep -v'enmr.eu' | cut -d',' -f3)
+echo Number of VMs run=$(python accounting.py -i https://accounting.egi.eu/egi/cloud/number_of_virtual_machines/SITE/VO/$start/$end/custom-enmr.eu/onlyinfrajobs/CSV 2>/dev/null | grep Total | grep -v'enmr.eu' | cut -d',' -f3)
+echo
